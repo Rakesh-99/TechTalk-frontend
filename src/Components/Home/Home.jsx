4c6c19e18bbf9e2ab0723063c3effe89fab5bc12
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2023-10-19 00:47:27 +0530

    conditional rendering of username

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
 
     const getusername = sessionStorage.getItem('username');
-    console.log(getusername);
+
 
     return (
         <div className="">
@@ -16,8 +16,9 @@ const Home = () => {
             <div className="h-screen bg-gray-950 flex px-10 py-5 w-full max-[760px]:flex-col max-[760px]:justify-center max-[760px]:items-center">
 
                 <div className="w-1/2 flex items-center justify-center flex-col max-[760px]:w-full">
-
-                    <div className="py-5"><h1 className='text-white text-xl sm:text-3xl font-semibold'>Hello<span className='text-xl sm:text-3xl  text-white bg-violet-800 rounded-md ml-3 px-3'>"{getusername}",</span></h1></div> 
+                      {getusername ?
+                        <div className="py-5"><h1 className='text-white text-xl sm:text-3xl font-semibold'>Hello<span className='text-xl sm:text-3xl  text-white bg-violet-800 rounded-md ml-3 px-3'>"{getusername}",</span></h1></div>
+                        : <></>}
 
                     <p className='text-white text-sm sm:text-xl '>Welcome to my <span className='text-violet-300'>blog</span> . This is where I write about <span className='text-violet-300'>technology</span> , new <span className='text-violet-300'>developments</span> .Just starting off with my site, stay tuned to read more soon.</p>
 
